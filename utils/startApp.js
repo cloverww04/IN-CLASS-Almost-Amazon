68ddaab1e511b22189ec71d347583f67031b4249
Nathan Clover <cloverww04@gmail.com>
2023-02-27 22:29:53 -0600

    added user instead of firebase key

--- utils/startApp.js
+++ utils/startApp.js
@@ -9,16 +9,16 @@ import { showBooks } from '../pages/books';
 import { showAuthors } from '../pages/authors';
 import { getAuthors } from '../api/authorData';
 
-const startApp = () => {
-  domBuilder(); // BUILD THE DOM
-  domEvents(); // ADD THE EVENT LISTENTERS TO THE DOM
-  formEvents(); // ADD FORM EVENT LISTENTERS TO THE DOM
-  navBar(); // DYNAMICALLY ADD THE NAV
+const startApp = (user) => {
+  domBuilder(user); // BUILD THE DOM
+  domEvents(user); // ADD THE EVENT LISTENTERS TO THE DOM
+  formEvents(user); // ADD FORM EVENT LISTENTERS TO THE DOM
+  navBar(user); // DYNAMICALLY ADD THE NAV
   logoutButton(); // ADD THE LOGOUT BUTTON COMPONENT
-  navigationEvents(); // ATTACH THE EVENT LISTENERS TO THE NAVBAR
+  navigationEvents(user); // ATTACH THE EVENT LISTENERS TO THE NAVBAR
 
   // TODO: Put all books on the DOM on App load
-  getBooks().then((books) => showBooks(books));
+  getBooks(user.uid).then((books) => showBooks(books));
   getAuthors().then((authors) => showAuthors(authors));
 };
 
